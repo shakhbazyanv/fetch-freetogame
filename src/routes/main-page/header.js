6a681c686dc9d7a3b432763fdcc7aa2261b3c61a
Shakhbazyan Vae <106478531+shakhbazyanv@users.noreply.github.com>
2023-08-31 21:12:42 +0300

    Update header.js

--- src/routes/main-page/header.js
+++ src/routes/main-page/header.js
@@ -71,57 +71,6 @@ function Header() {
                             <MenuItem value={sortByTypes.relevance}>Relevance</MenuItem>
                         </Select>
                     </FormControl>
-                        {/* <InputLabel id="demo-simple-select-standard-label">Genre</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-standard-label"
-                            id="demo-simple-select-standard"
-                            defaultValue={genreTypes.none}
-                            onChange={e => dispatch.filters.setGenre(e.target.value)}
-                            label="Genre"
-                            >
-                            <MenuItem value="">
-                                <em>Genre</em>
-                            </MenuItem>
-                            <MenuItem value={genreTypes.none}>None</MenuItem>
-                            <MenuItem value={genreTypes.shooter}>Shooter</MenuItem>
-                            <MenuItem value={genreTypes.mmorpg}>MMORPG</MenuItem>
-                            <MenuItem value={genreTypes.sports}>Sports</MenuItem> */}
-                        {/* <InputLabel variant="standard" htmlFor="uncontrolled-native" sx={{ marginLeft: '113px' }}>
-                            Platrform
-                        </InputLabel>
-                        <NativeSelect
-                            sx={{ marginLeft: '10px' }}
-                            onChange={e => dispatch.filters.setPlatform(e.target.value)}
-                            defaultValue={platformTypes.all}
-                            inputProps={{
-                            name: 'Platform',
-                            }}
-                        >
-                            <MenuItem value={platformTypes.all}>All</MenuItem>
-                            <MenuItem value={platformTypes.pc}>PC</MenuItem>
-                            <MenuItem value={platformTypes.browser}>Browser</MenuItem>
-                        </NativeSelect>
-                        <InputLabel variant="standard" htmlFor="uncontrolled-native" sx={{ marginLeft: '216px' }}>
-                            Sort
-                        </InputLabel> */}
-
-
-
-
-                        {/* <NativeSelect
-                            sx={{ marginLeft: '10px' }}
-                            onChange={e => dispatch.filters.setSortBy(e.target.value)}
-                            defaultValue={sortByTypes.alphabetical}
-                            inputProps={{
-                            name: 'sortBy',
-                            }}
-                        >
-                            <MenuItem value={sortByTypes.alphabetical}>Alphabetical</MenuItem>
-                            <MenuItem value={sortByTypes.popularity}>Popularity</MenuItem>
-                            <MenuItem value={sortByTypes.releaseDate}>Release date</MenuItem>
-                            <MenuItem value={sortByTypes.relevance}>Relevance</MenuItem>
-                        </NativeSelect>
-                        </FormControl>*/}
                     </div> 
                     <Typography variant="h6" component="div">
                         Game List
